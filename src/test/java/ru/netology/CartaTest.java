5ab4a52515f6cb5ddbd22fc5ab7dbbd61a8698de
Oleg <ovt1966@gmail.com>
2023-06-21 19:24:14 +0300

    Add test

---
+++ src/test/java/ru/netology/CartaTest.java
@@ -0,0 +1,39 @@
+package ru.netology;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+class CartaTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll(){
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp(){driver = new ChromeDriver();}
+
+    @AfterEach
+    void tearDown(){
+        driver.quit();
+        driver = null;
+    }
+    @Test
+    void shouldTestCarta() throws InterruptedException {
+// загрузить страницу
+        driver.get("http://localhost:9999/");
+        Thread.sleep(5000);
+// поиск элементов
+// взаимодействие с элементами
+
+    }
+
+
+}
