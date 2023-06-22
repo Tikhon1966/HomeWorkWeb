1179e46200d0ad14b5924b24e69a95e59155df3e
Oleg <ovt1966@gmail.com>
2023-06-22 23:05:13 +0300

    Change test

--- src/test/java/ru/netology/CartaTest.java
+++ src/test/java/ru/netology/CartaTest.java
@@ -5,34 +5,45 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CartaTest {
 
     private WebDriver driver;
 
     @BeforeAll
-    static void setUpAll(){
+    static void setUpAll() {
         WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
-    void setUp(){driver = new ChromeDriver();}
+    void setUp() {
+        driver = new ChromeDriver();
+    }
 
     @AfterEach
-    void tearDown(){
+    void tearDown() {
         driver.quit();
         driver = null;
     }
-    @Test
-    void shouldTestCarta() throws InterruptedException {
-// загрузить страницу
-        driver.get("http://localhost:9999/");
-       // Thread.sleep(5000);
-// поиск элементов
-// взаимодействие с элементами
 
+    @Test
+    void shouldTestCarta() {
+        driver.get("http://localhost:9999/");
+        List<WebElement> elements = (List<WebElement>) driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Петров Иван");
+        elements.get(1).sendKeys("+79161111111");
+        driver.findElement(By.className("checkbox__text")).click();
+        driver.findElement(By.className("button__text")).click();
+        String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
 
